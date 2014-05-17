3c7b0a9868bbddeb5e72d66147ef53f7b0a18e8a
Benjamin Attal <attal.benjamin@gmail.com>
2014-05-16 22:47:03 -0400

    Get rid of username.  Replace with name instead.  Usernames are useless if there is an email as well.

--- db/migrate/20140505215223_create_users.rb
+++ db/migrate/20140505215223_create_users.rb
@@ -1,15 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
     def change
         create_table :users do |t|
+            t.string :name, :default => ''
             t.string :email_encrypted
-            t.string :username
             t.string :password_encrypted
-            t.integer :zip_code
+            t.integer :zip_code, :default => -1
 
             t.timestamps
         end
 
-        add_index :users, [:created_at, :username]
         add_index :users, [:created_at, :zip_code]
     end
 end
