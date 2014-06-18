ef432eddb1ed3b52f52316157a0e5ac5c41d1cdd
Benjamin Attal <attal.benjamin@gmail.com>
2014-06-18 03:03:33 -0400

    Force production

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ENV['RAILS_ENV'] ||= 'production'
+
 if Rails.env.production?
     ENV['BASIC_AUTH_USERNAME'] = 'cartwheels'
     ENV['BASIC_AUTH_PASSWORD'] = '9Ca$#@368'
