fa912517f1981f7f1f6b6eab4565ff4001279912
Benjamin Attal <attal.benjamin@gmail.com>
2014-06-23 16:54:47 -0400

    Fix authentication to allow apps to get images

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    before_filter :authenticate_basic_http
+
     def sign_up_params
         params.require(:user).permit(:name, :email, :zip_code, :password,
             :password_confirmation, :provider, :uid)
