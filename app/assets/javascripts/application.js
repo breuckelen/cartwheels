801c132d8f66952e27c8b7a9fdfaf041caaa1c44
Benjamin Attal <attal.benjamin@gmail.com>
2014-06-26 16:28:41 -0400

    Make css scalable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery_mousewheel
 //= require turbolinks
 //= require underscore
 //= require bootstrap
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,5 @@
 (function() {
     var lastVertPos = $(window).scrollTop();
-
     var onScroll = function(e) {
         var currVertPos = $(window).scrollTop();
         var delta = currVertPos - lastVertPos;
@@ -11,15 +10,20 @@
         lastVertPos = currVertPos;
     }
 
+    var catchMapScroll = function(e) {
+        e.stopPropagation();
+    }
+
+    var cartMap;
     var ready = function(e) {
-        new GMaps({
+        cartMap = new GMaps({
             div: '#map-div',
             lat: -12.043333,
-            lng: -77.028333
+            lng: -77.028333,
+            scrollwheel: false
         });
+
+        $(window).mousewheel(onScroll);
     };
-
     $(document).ready(ready);
-
-    $(window).scroll(onScroll);
 })();
