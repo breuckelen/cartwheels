12b845100a2b09b3177b43af896fee7ea1035a94
Benjamin Attal <attal.benjamin@gmail.com>
2014-07-03 02:31:34 -0400

    remove default from description in checkins

--- db/migrate/20140703020956_add_description_to_checkin.rb
+++ db/migrate/20140703020956_add_description_to_checkin.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToCheckin < ActiveRecord::Migration
   def change
-    add_column :checkins, :description, :text, default: ''
+    add_column :checkins, :description, :text
   end
 end
