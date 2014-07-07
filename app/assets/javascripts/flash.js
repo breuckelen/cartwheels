74ada6a0157e7b74fc412cd27b2098b0d4f5abbd
Benjamin Attal <attal.benjamin@gmail.com>
2014-07-06 23:51:28 -0400

    Remove unecessary files

--- app/assets/javascripts/flash.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var ready = function() {
-        $('.alert').delay(2000).fadeOut(500);
-    }
-
-    $(document).ready(ready);
-    $(document).on('page:load', ready);
-})();
--- db/migrate/20140707032810_add_another_description_to_carts.rb
+++
@@ -1,9 +0,0 @@
-class AddAnotherDescriptionToCarts < ActiveRecord::Migration
-  def up
-      add_column :carts, :description, :string, default: ''
-  end
-
-  def down
-      remove_column :carts, :description
-  end
-end
